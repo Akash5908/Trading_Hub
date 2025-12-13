d7e3a432deef29ebc127cb7531e4361a736c8f3a
Akash5908 <venomkr020@gmail.com>
2025-12-13 15:43:03 +0530

    Modification in redux

--- http_server/dist/src/utils/bcrypt.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt";
-export function hashPassword(password) {
-    // Using bcrypt for hashing password
-    const saltRounds = 10;
-    const salt = bcrypt.genSaltSync(saltRounds);
-    const hashedPassword = bcrypt.hashSync(password, salt);
-    return hashedPassword;
-}
-//# sourceMappingURL=bcrypt.js.map
--- price_poller/dist/redis.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function pushToRedis(redis: any, trades: any): Promise<void>;
-//# sourceMappingURL=redis.d.ts.map
